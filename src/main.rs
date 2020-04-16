a68c283c6be29581c3a4ece632425718957dfd74
Nick Machnik <nick.machnik@gmail.com>
2020-04-16 21:51:55 +0200

    Add basic constructor and update routines for Entry and Table structs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+extern crate chrono;
 extern crate clap;
 extern crate colored;
+extern crate rayon;
+
+use chrono::prelude::*;
+use colored::Colorize;
+
+mod table;
 
 fn main() {
-    println!("Hello, world!");
+    println!("{}", Local::now().to_string().on_blue().red());
 }
--- src/table.rs
+++ src/table.rs
@@ -1,12 +1,75 @@
 //! The `table` mod contains structs that hold the actual
 //! data written, stored and read by the application.
 
+use chrono::{Date, Duration, Utc};
+use rayon::prelude::*;
+
 #[derive(Debug)]
 struct Table {
     entries: Vec<Entry>,
 }
 
+impl Table {
+    fn new() -> Self {
+        Table {
+            entries: Vec::new(),
+        }
+    }
+
+    fn add_entry(&mut self, entry: Entry) {
+        self.entries.push(entry);
+    }
+
+    fn update_entries_par(&mut self) {
+        self.entries
+            .par_iter_mut()
+            .for_each(|e| e.update_remaining_time());
+        self.entries
+            .sort_by(|a, b| b.remaining_time.cmp(&a.remaining_time));
+    }
+}
+
 #[derive(Debug)]
 struct Entry {
-    field: Type,
+    pub name: String,
+    // The chat interval in days
+    pub interval: Duration,
+    last_contact: Date<Utc>,
+    remaining_time: i64,
+}
+
+impl Entry {
+    fn new(name: String, interval: Duration, last_contact: Date<Utc>) -> Self {
+        Entry {
+            name,
+            interval,
+            last_contact,
+            remaining_time: (interval.num_days()
+                - Utc::now()
+                    .date()
+                    .signed_duration_since(last_contact)
+                    .num_days()),
+        }
+    }
+
+    fn update_remaining_time(&mut self) {
+        self.remaining_time = self.interval.num_days()
+            - Utc::now()
+                .date()
+                .signed_duration_since(self.last_contact)
+                .num_days();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_remaining_time() {
+        let mut e1 = Entry::new("Martin".to_string(), Duration::days(30), Utc::now().date());
+        assert_eq!(30, e1.remaining_time);
+        e1.update_remaining_time();
+        assert_eq!(30, e1.remaining_time);
+    }
 }
