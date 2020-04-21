fb2dcafda75bea37547c2538b359b16dbb0f505e
Nick Machnik <nick.machnik@gmail.com>
2020-04-21 22:40:45 +0200

    Add skeletons for subcommands

---
+++ src/subcommands.rs
@@ -0,0 +1,22 @@
+use clap::ArgMatches;
+use std::ffi::OsStr;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+
+pub fn add(args: ArgMatches) {
+    unimplemented!()
+    // error!("Attempted to index compressed file: {:?}", inpath);
+    // info!("All done.");
+}
+
+pub fn remove(args: ArgMatches) {
+    unimplemented!()
+}
+
+pub fn modify(args: ArgMatches) {
+    unimplemented!()
+}
+
+pub fn view(args: ArgMatches) {
+    unimplemented!()
+}
