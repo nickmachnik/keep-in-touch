03a99ca8da2666b3aa23f5ad5204324851f20ad2
Nick Machnik <nick.machnik@gmail.com>
2020-04-29 18:14:39 +0200

    remove `add_existing()` test

--- tests/cli.rs
+++ tests/cli.rs
@@ -3,17 +3,6 @@ use predicates::prelude::*;
 use std::process::Command;
 
 #[test]
-fn add_existing() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("kit")?;
-    cmd.arg("add").arg("Martin").arg("30").arg("now");
-    cmd.assert()
-        .failure()
-        .stderr(predicate::str::contains("already used"));
-
-    Ok(())
-}
-
-#[test]
 fn add_wrong_date_format() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("kit")?;
     cmd.arg("add").arg("Martin").arg("30").arg("20baba");
