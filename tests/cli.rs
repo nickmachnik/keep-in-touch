dfdf4cab691b35cc0519a06651449f9fe4aff4c8
Nick Machnik <nick.machnik@gmail.com>
2020-04-29 18:25:44 +0200

    Remove failure tests

--- tests/cli.rs
+++ tests/cli.rs
@@ -3,39 +3,6 @@ use predicates::prelude::*;
 use std::process::Command;
 
 #[test]
-fn add_wrong_date_format() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("kit")?;
-    cmd.arg("add").arg("Martin").arg("30").arg("20baba");
-    cmd.assert()
-        .failure()
-        .stderr(predicate::str::contains("Parsing the date string failed"));
-
-    Ok(())
-}
-
-#[test]
-fn add_wrong_interval_format() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("kit")?;
-    cmd.arg("add").arg("Martin").arg("bubu").arg("now");
-    cmd.assert().failure().stderr(predicate::str::contains(
-        "Parsing the interval field failed",
-    ));
-
-    Ok(())
-}
-
-#[test]
-fn remove_non_existing() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("kit")?;
-    cmd.arg("remove").arg("Daniel");
-    cmd.assert()
-        .failure()
-        .stderr(predicate::str::contains("is not in the list"));
-
-    Ok(())
-}
-
-#[test]
 fn whole_workflow() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("kit")?;
     cmd.arg("add").arg("X").arg("30").arg("now");
